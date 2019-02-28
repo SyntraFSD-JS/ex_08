190de7022e4a47c7b2d823e44d0b71ffbd5ce634
Korneel Eeckhout <korneel@pienter.tech>
2019-02-28 21:03:34 +0100

    Init

---
+++ README.md
@@ -0,0 +1,59 @@
+# Oefening 07 - HEREXAMEN
+
+## Voorbereiding:
+Clone de git-repository uit github en maak je eigen branch.
+1. Kopieer de url via github naar je klembord:  
+   ![Copy url](https://raw.githubusercontent.com/SyntraFSD-JS/ex-01/master/images/copy_github.png "Copy url to clipboard")
+
+Gebruik de terminal uit VSCode 
+(zorg dat je in de juiste folder, die waar je alle oefeningen opslaat, zit in VSCode)
+ om de volgende commands uit te voeren.
+(vervang `[naam]` door je eigen naam)
+
+1. `git clone https://github.com/SyntraFSD-JS/ex-08.git ex-07`
+2. `cd ex-08`
+3. `git checkout -b [naam]`
+4. `git push --set-upstream origin [naam]`
+
+Push regelmatig de changes die je maakt naar github
+
+1. Commit je changes:
+   1. Druk `Ctrl-Shift-G` om de Source Control tab te openen
+   2. Geef een beschrijving van de changes die je deed en duw `Ctrl-Enter`
+2. Push je changes:
+   1. Druk `Ctrl-Shift-P` om het Command Palette te openen
+   2. Zoek naar `Git: Push` en druk op `Enter` om uit te voeren 
+   
+Pull regelmatig om opmerkingen of aanpassingen van mij binnen te halen
+1. Druk `Ctrl-Shift-P` om het Command Palette te openen
+2. Zoek naar `Git: Pull` en druk op `Enter` om uit te voeren 
+     
+## Structuur
+De oefeningen bevinden zich in een aparte map met de naam `ex[oef. nummer]_[oef. naam]`
+Daarin zitten alle bestanden die je nodig hebt om de oefening te maken, meestal is dit een `index.html` en een `script.js` bestand.
+Het script bestand zal al gelinkt zijn aan het html bestand. Zie:
+```html
+<script src="script.js"></script>
+```
+Normaal moet je niet in het html bestand meer werken maar enkel in `script.js`.
+Ook daar zal meestal al een basisstructuur aanwezig zijn waarbij in comments staat waar je zelf dingen moet aanvullen.
+
+Om het resultaat van je oefening te bekijken open je het `index.html` bestand in chrome.
+In deze module gebruiken we allemaal chrome zodat we allemaal hetzelfde resultaat hebben.
+
+## Oefeningen:
+### Oefening 08: Mastermind
+#### Doel:
+Het doel van de oefening is het programmeren van een 'MasterMind' spel.
+- Jullie kunnen het spel spelen door `solution/index.html` te openen in je browser.
+(Doe geen moeite, het solution/script.js bestand is onleesbaar gemaakt)
+- Bestudeer verder de commentaren in het `script.js` bestand aandachtig.
+
+Vergeet niet regelmatig te `commiten` en let op de correcte indentering van jullie bestand.
+
+Veel geluk.
+
+
+ 
+   
+      
---
+++ ex08_mastermind/script.js
@@ -0,0 +1,133 @@
+// .../80
+// .../4
+// ten points for:
+// pushing regularly (every 30 minutes)
+// clean code (indentation)
+
+
+// ********** Model **********
+// .../7
+// QuerySelectors
+const solutionContainer;
+const triesContainer;
+const tryInputSection;
+const tryInputs;
+const trySubmitBtn;
+const messageContainer;
+const winnerSubmitBtn;
+
+// Save the code/solution of the game in this variable
+// Hint: save your code in an array
+let code = null;
+
+// ********** View **********
+// .../1
+function hideCode() {
+  // hide the code
+  // have a look at the html to find out how
+}
+
+// .../1
+function showCode() {
+  // show the code
+  // have a look at the html to find out how
+}
+
+// .../4
+function drawCode(codeArray) {
+  // Draw the code  array into solution-container
+  // Don't forget to empty the container first
+}
+
+// .../1
+function emptyTriesContainer() {
+  // empty the tries container
+}
+
+// .../6
+function drawNewTry(tryArray, correctNumberCount, correctPlaceCount) {
+  // append a new try to the triesContainer (check the html file)
+  // don't forget the winner class if all numbers are correct
+}
+
+// .../3
+function emptyTryInputs() {
+  // empty the try input fields
+}
+
+// .../2
+function showTryInput() {
+  // show tryInputContainer
+  // hide messageContainer
+  // use dont-show class
+}
+
+// .../2
+function showMessage() {
+  // show tryInputContainer
+  // hide messageContainer
+  // use dont-show class
+}
+
+// ********** Update **********
+// .../3
+function randomNumber() {
+  // this function returns a random number between 1 and 6
+}
+
+// .../4
+function generateNewCode() {
+  // this function generates and returns a new code (array of 4 random numbers)
+}
+
+// .../4
+function validateTryInputs() {
+  // validate all four try input field
+  // make sure all numbers are between 1 and 6
+  // return true or false
+}
+
+// .../6
+function initGame() {
+  // Reset game
+}
+
+// .../4
+function generateTryArray() {
+  // generate an array with the four values from the input fields
+  // make sure they are inserted in the array as an int not a string
+}
+
+// .../10
+function calculateCorrectNumberCount(codeArray, tryArray) {
+  // Calculate the amount of correct numbers in the tryArray
+  // A correct number does not have to be in the correct spot
+}
+
+// .../6
+function calculateCorrectPlaceCount(codeArray, tryArray) {
+  // Calculate the amount of numbers matching the code
+  // The numbers have to be identical and in the same place
+}
+
+// ********** Events **********
+window.addEventListener('load', function () {
+  initGame();
+});
+
+// .../8
+// on submit btn click
+// check if the input are valid
+// if the inputs are not valid do nothing
+// if they are:
+// generate the try array
+// calculate the correct number count
+// calculate the correct place count
+// draw the try in html
+// if the try is a winner show the winner btn
+// and show the code
+// of course you will need to use the correct update/view functions
+
+// .../3
+// on winner btn click
+// init a new game
