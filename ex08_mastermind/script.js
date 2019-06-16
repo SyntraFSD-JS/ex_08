4fa3e66026684ab0d916b722d66af60d7febb7b6
Korneel Eeckhout <korneel@pienter.tech>
2019-06-16 12:15:41 +0200

    Quote

--- ex08_mastermind/script.js
+++ ex08_mastermind/script.js
@@ -1,12 +1,12 @@
-// .../80
-// .../4
+// 13/80
+// 2/4
 // ten points for:
 // pushing regularly (every 30 minutes)
 // clean code (indentation)
 
 
 // ********** Model **********
-// .../7
+// 7/7
 // QuerySelectors
 const solutionContainer = document.querySelector('#solution-container');
 const triesContainer = document.querySelector('#tries-container');
@@ -24,7 +24,7 @@ let code = [Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) 
 const codeArray = document.querySelectorAll('.solution-option');
 
 // ********** View **********
-// .../1
+// 1/1
 
 function hideCode() {
   // hide the code
@@ -32,7 +32,7 @@ function hideCode() {
   solutionContainer.classList.add('hidden');
 }
 
-// .../1
+// 1/1
 function showCode() {
   // show the code
   // have a look at the html to find out how
@@ -41,7 +41,7 @@ function showCode() {
 }
 
 
-// .../4
+// 0/4
 function drawCode(codeArray) {
   // Draw the code array into solution-container
   // Don't forget to empty the container first
@@ -57,13 +57,13 @@ function drawCode(codeArray) {
 }
 
 
-// .../1
+// 1/1
 function emptyTriesContainer() {
   // empty the tries container
   triesContainer.innerHTML = '';
 }
 
-// .../6
+// 1/6
 function drawNewTry(tryArray, correctNumberCount, correctPlaceCount) {
   // append a new try to the triesContainer (check the html file)
   // don't forget the winner class if all numbers are correct
@@ -85,19 +85,19 @@ function drawNewTry(tryArray, correctNumberCount, correctPlaceCount) {
 
 }
 
-// .../3
+// 0/3
 function emptyTryInputs() {
   // empty the try input fields
 }
 
-// .../2
+// 0/2
 function showTryInput() {
   // show tryInputContainer
   // hide messageContainer
   // use dont-show class
 }
 
-// .../2
+// 0/2
 function showMessage() {
   // show tryInputContainer
   // hide messageContainer
@@ -105,41 +105,41 @@ function showMessage() {
 }
 
 // ********** Update **********
-// .../3
+// 0/3
 function randomNumber() {
   // this function returns a random number between 1 and 6
 }
 
-// .../4
+// 0/4
 function generateNewCode() {
   // this function generates and returns a new code (array of 4 random numbers)
 }
 
-// .../4
+// 0/4
 function validateTryInputs() {
   // validate all four try input field
   // make sure all numbers are between 1 and 6
   // return true or false
 }
 
-// .../6
+// 0/6
 function initGame() {
   // Reset game
 }
 
-// .../4
+// 0/4
 function generateTryArray() {
   // generate an array with the four values from the input fields
   // make sure they are inserted in the array as an int not a string
 }
 
-// .../10
+// 0/10
 function calculateCorrectNumberCount(codeArray, tryArray) {
   // Calculate the amount of correct numbers in the tryArray
   // A correct number does not have to be in the correct spot
 }
 
-// .../6
+// 0/6
 function calculateCorrectPlaceCount(codeArray, tryArray) {
   // Calculate the amount of numbers matching the code
   // The numbers have to be identical and in the same place
